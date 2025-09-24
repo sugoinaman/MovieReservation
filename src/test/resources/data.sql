30410f7ae678b11b1b6e528c6dba200a8b047c99
sugoi <jhanaman571@gmail.com>
2025-09-24 21:03:12 +0530

    awnjkdHBAWD

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,5 @@
--- INSERT INTO PERSON (id, username, email, password, role)
--- VALUES (1, 'john_doe', 'john@example.com', 'password123', 'USER'),
---        (2, 'jane_admin', 'jane@example.com', 'securePass!', 'ADMIN'),
---        (3, 'alex_smith', 'alex@example.com', 'alexPass!', 'USER'),
---        (4, 'emily_white', 'emily@example.com', 'emily123', 'USER');
+INSERT INTO PERSON (id, username, email, password, role)
+VALUES (1, 'john_doe', 'john@example.com', 'password123', 'USER'),
+       (2, 'jane_admin', 'jane@example.com', 'secret!', 'ADMIN'),
+       (3, 'alex_smith', 'alex@example.com', 'alexPass!', 'USER'),
+       (4, 'emily_white', 'emily@example.com', 'emily123', 'USER');
