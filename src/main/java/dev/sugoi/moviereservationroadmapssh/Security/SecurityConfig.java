545149de2daa861556a06998144cf93c7ee6fc64
sugoi <jhanaman571@gmail.com>
2025-09-26 20:05:01 +0530

    Uh

--- src/main/java/dev/sugoi/moviereservationroadmapssh/Security/SecurityConfig.java
+++ src/main/java/dev/sugoi/moviereservationroadmapssh/Security/SecurityConfig.java
@@ -7,49 +7,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(request -> request
-
-                        .requestMatchers("/user/**").hasRole("ADMIN")
-                        .requestMatchers("/user/signup").permitAll()
-                        .requestMatchers("/user").authenticated()
-
+                .authorizeHttpRequests(authorizeHttp -> {
+                            authorizeHttp.requestMatchers("/user/signup").permitAll();
+                            authorizeHttp.anyRequest().authenticated();
+                        }
                 )
-
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable());
         return http.build();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    PasswordEncoder passwordEncoder() {
+        return new NoOpPasswordEncoder();
     }
 
-    @Bean
-    UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
-
-        User.UserBuilder users = User.builder();
-        UserDetails johndoe = users
-                .username("john_doe")
-                .password("password123")
-                .roles(String.valueOf(Role.USER))
-                .build();
-
-        return new InMemoryUserDetailsManager(johndoe);
-    }
 
 }
