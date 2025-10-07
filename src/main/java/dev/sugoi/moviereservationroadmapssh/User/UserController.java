13a5e1f28c52704a3d4d3a3119e87ae36abe66c3
sugoi <jhanaman571@gmail.com>
2025-10-07 16:58:41 +0530

    Uh

--- src/main/java/dev/sugoi/moviereservationroadmapssh/User/UserController.java
+++ src/main/java/dev/sugoi/moviereservationroadmapssh/User/UserController.java
@@ -1,21 +1,21 @@
 package dev.sugoi.moviereservationroadmapssh.User;
 
-import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.PrincipalMethodArgumentResolver;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -24,6 +24,7 @@ public class UserController {
 
     @GetMapping("/getAllUsers")
     ResponseEntity<List<User>> getAllUsers() {
+        log.info("get all user endpoint accessed");
         List<User> listOfUsers = userService.getAllUsers();
         return ResponseEntity.ok(listOfUsers);
     }
@@ -31,34 +32,38 @@ public class UserController {
 
     @GetMapping("/{id}")
     ResponseEntity<User> getUserById(@PathVariable Integer id) {
-
+        log.info("get user by id: {} used", id);
         Optional<User> optionalUser = userService.getUserById(id);
         return optionalUser
-                .map(user -> ResponseEntity.ok(user)).
+                .map(ResponseEntity::ok).
                 orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 
     }
 
     @PostMapping("/signup")
     ResponseEntity<String> addUser(@RequestBody User user, UriComponentsBuilder ucb) {
-        User userToBeSaved = new User(user.getUserName(), user.getEmail(), user.getPassword(),Role.USER,List.of());
+
+        log.info("new user trying to sign up with credentials:{} ", user.toString());
+        User userToBeSaved = new User(user.getUserName(), user.getEmail(), user.getPassword(), Role.USER, List.of());
 
         userService.addUser(userToBeSaved);
         URI location = ucb
                 .path("user/get/{id}")
-                .buildAndExpand(userToBeSaved.getId())
+                .buildAndExpand(userToBeSaved.getid())
                 .toUri();
         return ResponseEntity.created(location).build();
     }
 
     @PutMapping("/update/{id}")
     ResponseEntity<Void> updateUser(@PathVariable Integer id, @RequestBody User newUser) {
-        userService.modifyUser(newUser,id);
+        log.info("trying to modify user with id {}, new user: {} ", id, newUser.toString());
+        userService.modifyUser(newUser, id);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/delete/{id}")
     ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        log.info("deleting user with id: {}", id);
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
