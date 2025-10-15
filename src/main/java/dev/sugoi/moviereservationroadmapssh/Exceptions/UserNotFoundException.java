9d86d5c414aa560beb52bdb50553273c3c2a47bd
sugoi <sugoi571@gmail.com>
2025-10-15 17:57:18 +0530

    test containers

--- src/main/java/dev/sugoi/moviereservationroadmapssh/Exceptions/UserNotFoundException.java
+++ src/main/java/dev/sugoi/moviereservationroadmapssh/Exceptions/UserNotFoundException.java
@@ -1,6 +1,9 @@
 package dev.sugoi.moviereservationroadmapssh.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
