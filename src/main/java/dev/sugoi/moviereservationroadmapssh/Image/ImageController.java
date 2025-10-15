f2ce434e0ab768fcdee3c50389dd45dc2901cefd
sugoi <jhanaman571@gmail.com>
2025-10-15 19:06:57 +0530

    uh

---
+++ src/main/java/dev/sugoi/moviereservationroadmapssh/Image/ImageController.java
@@ -0,0 +1,38 @@
+package dev.sugoi.moviereservationroadmapssh.Image;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
+@RestController
+public class ImageController {
+
+    private final ImageRepository imageRepository;
+
+    ImageController(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    @PostMapping
+    public Long saveImage(@RequestBody MultipartFile image) throws IOException {
+        Image dbimage = new Image();
+        dbimage.setName(image.getName());
+        dbimage.setContent(image.getBytes());
+
+        return imageRepository.save(dbimage).getId();
+    }
+
+    @GetMapping
+    Resource downloadImage(@PathVariable Long id) {
+        byte[] image = imageRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND))
+                .getContent();
+
+        return new ByteArrayResource(image);
+    }
+}
