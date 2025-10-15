7e452ed4727bda96c3c5c4fad14a4805dad164f6
sugoi <jhanaman571@gmail.com>
2025-10-15 21:53:49 +0530

    uh

---
+++ src/test/java/dev/sugoi/moviereservationroadmapssh/User/ImageControllerTest.java
@@ -0,0 +1,8 @@
+package dev.sugoi.moviereservationroadmapssh.User;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ImageControllerTest {
+
+}
